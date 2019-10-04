16ff65f4ec241c0cd044aa4bc6b19646b7a4b1ec
Josh <jwo@salesforce.com>
2019-10-04 16:30:42 -0700

    add comments

--- src/test/java/com/salesforce/mc/as/infr/MRInputOutputLogParserTest.java
+++ src/test/java/com/salesforce/mc/as/infr/MRInputOutputLogParserTest.java
@@ -11,9 +11,12 @@ public class MRInputOutputLogParserTest {
 
     @Test
     public void main() throws Exception {
+        //test run
         String path = MRInputOutputLogParser.class.getClassLoader()
                 .getResource("log-sample.csv").toString();
-        MRInputOutputLogParser.main(new String[]{"", "file:/Users/jwo/Downloads/search-results-2019-06-07T10_21_17.957-0700.csv"});
+
+        //Use this for your real data parsing.
+        // MRInputOutputLogParser.main(new String[]{"", "file:/Users/jwo/Downloads/search-results-2019-06-07T10_21_17.957-0700.csv"});
     }
 
 
